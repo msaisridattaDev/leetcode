b54babbf047f7ddaca028cd2ebdaa9f44cd82c31
msaisridattaDev <121572604+msaisridattaDev@users.noreply.github.com>
2023-09-20 21:21:54 +0530

    Time: 5568 ms (7.74%) | Memory: 21.4 MB (10.36%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,39 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        d=sorted(nums)
+
+        m=[]
+
+        while(len(d)!=0):
+            u=len(d)-1
+            c=-(d.pop(-1))
+            p1=0
+            p2=len(d)-1
+
+            while(p1<p2):
+
+                s=d[p1]+d[p2]
+
+                if s==c:
+                    h=[d[p1],d[p2],-(c)]
+                    if h in m:
+                        pass
+                    else:   
+                        m+=[h]
+                    p2-=1
+                    p1+=1
+
+                else:
+                    #print(d[p1],c-d[p1],d[p2],-c)
+                    if d[p2]>= c-d[p1] >= d[p1]:
+                        #print("yes entered ",d[p1],c-d[p1],d[p2])
+                        p2-=1
+                    else:
+                        p1+=1
+
+        return m
+        
+
+
+        
