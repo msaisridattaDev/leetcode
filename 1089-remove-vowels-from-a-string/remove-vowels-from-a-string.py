7200bdc6ae5c172d1e80bdab85c883bcc0a65419
msaisridattaDev <121572604+msaisridattaDev@users.noreply.github.com>
2024-01-05 00:23:10 +0530

    Time: 32 ms (86.76%) | Memory: 17.5 MB (7.46%) - LeetSync

--- 1089-remove-vowels-from-a-string/remove-vowels-from-a-string.py
+++ 1089-remove-vowels-from-a-string/remove-vowels-from-a-string.py
@@ -3,9 +3,8 @@ class Solution:
         
         p=""
         for i in s:
-            if i=="a" or i=="e" or i=="i" or i=="o" or i=="u" :
-                pass
-            else:
+            if not(i=="a" or i=="e" or i=="i" or i=="o" or i=="u") :
                 p+=i
+            
 
         return p
