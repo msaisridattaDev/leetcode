ebef6bb8b72bc903d7286f20a8c880930e25f2c8
msaisridattaDev <121572604+msaisridattaDev@users.noreply.github.com>
2024-05-24 23:48:26 +0530

    Time: 76 ms (18.38%) | Memory: 17 MB (11.15%) - LeetSync

---
+++ 1074-high-five/high-five.py
@@ -0,0 +1,29 @@
+import heapq
+class Solution:
+    def highFive(self, items: List[List[int]]) -> List[List[int]]:
+        c={}
+
+        for i in items:
+
+            if i[0] in c.keys():
+                c[i[0]].append(i[1])
+            else:
+                c[i[0]]=[i[1]]
+
+        r=[]
+        for j in c.keys():
+
+            heapq.heapify(c[j])
+
+            for k in range(5):
+                s=heapq.nlargest(5,c[j])
+            
+            p=int(sum(s)/5)
+
+            r.append([j,p])
+
+        r.sort()
+        return r
+
+    
+
