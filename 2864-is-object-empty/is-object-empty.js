670caf5fdce60c7325d5c4f2bc2a14a52c9a9347
msaisridattaDev <121572604+msaisridattaDev@users.noreply.github.com>
2024-06-11 01:59:30 +0530

    Time: 50 ms (68.40%) | Memory: 49.3 MB (55.48%) - LeetSync

---
+++ 2864-is-object-empty/is-object-empty.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+
+    if (Object.keys(obj).length)
+    {
+        return false
+    
+    }
+    else{
+        return true
+    }
+
+
+    
+};
