9f05faf44d595000ae2e0c9c8daa06c85cd7922d
msaisridattaDev <121572604+msaisridattaDev@users.noreply.github.com>
2024-08-10 06:08:57 +0530

    Time: 522 ms (45.37%) | Memory: 42.5 MB (39.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,8 +1,6 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
 
-        
-
         s=text1
         r=text2
 
@@ -10,29 +8,23 @@ class Solution:
         n=len(r)
         
         dp=[]
-        for i in range(len(s)):
+        for i in range(m+1):
             temp=[]
-            for j in range(len(r)):
+            for j in range(n+1):
 
                 temp.append(0)
 
             dp.append(temp)
 
-        for i in range(len(s)):
+        for i in range(1,m+1):
         
-            for j in range(len(r)):
-                if s[i]==r[j]:
-
-                    if 0<=i-1<=m-1 and 0<=j-1<=n-1:
-                        dp[i][j]=dp[i-1][j-1]+1
-                    else:
-                        dp[i][j]=1
-
-                    
+            for j in range(1,n+1):
+                if s[i-1]==r[j-1]:
+                    dp[i][j]=dp[i-1][j-1]+1
                 else:
                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
 
-        return dp[m-1][n-1]
+        return dp[m][n]
 
         
         
