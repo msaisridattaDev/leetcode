8ad7292207bea208e51ac560371f56e216c43e3b
msaisridattaDev <121572604+msaisridattaDev@users.noreply.github.com>
2024-08-23 10:51:08 +0530

    Time: 119 ms (78.02%) | Memory: 20.6 MB (50.33%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self,intervals):
+        if not intervals:
+            return []
+        
+        # Step 1: Sort intervals based on the start time
+        intervals.sort(key=lambda x: x[0])
+        
+        merged = [intervals[0]]  # Start with the first interval
+        
+        # Step 3: Iterate through the sorted intervals
+        for current in intervals[1:]:
+            last_merged = merged[-1]
+            
+            if current[0] <= last_merged[1]:  # There is an overlap
+                # Merge the current interval with the last one in the merged list
+                merged[-1][1] =max(last_merged[1],current[1])
+            else:
+                # No overlap, add the current interval to the merged list
+                merged.append(current)
+        
+        return merged
