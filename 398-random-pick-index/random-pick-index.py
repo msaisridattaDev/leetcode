9105832b1564b44b7942e788eda376d06c413cb2
msaisridattaDev <121572604+msaisridattaDev@users.noreply.github.com>
2024-08-26 09:03:12 +0530

    Time: 289 ms (29.36%) | Memory: 26.4 MB (74.57%) - LeetSync

---
+++ 398-random-pick-index/random-pick-index.py
@@ -0,0 +1,34 @@
+import random
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.l=nums
+        self.c={}
+        self.t={}
+        for i,v in enumerate(self.l):
+            if v in self.c.keys():
+                self.c[v]+=[i]
+            else:
+                self.c[v]=[i]
+
+    def pick(self, target: int) -> int:
+
+        if target in self.c.keys() and target not in self.t.keys():
+            p=self.c[target]
+            random.shuffle(p)
+            self.c[target]=p
+            self.t[target]=0
+            return p[0]
+        elif target in self.c.keys() and target  in self.t.keys():
+            p=self.c[target]
+            j=self.t[target]
+            self.t[target]= (self.t[target]+1)% len(self.c[target])
+            return p[j]
+        
+
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
