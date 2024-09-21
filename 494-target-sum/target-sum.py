270f1ff63e7ca043ae5c6ccbd39261ea3d91442e
msaisridattaDev <121572604+msaisridattaDev@users.noreply.github.com>
2024-09-21 19:57:36 +0530

    Time: 205 ms (56.54%) | Memory: 17 MB (66.00%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,24 +1,17 @@
+##########3###########
+
 class Solution:
     def findTargetSumWays(self, nums, S):
         total = sum(nums)
-        memo = [[-1] * (2 * total + 1) for _ in range(len(nums))]
+        dp = [[0] * (2 * total + 1) for _ in range(len(nums))]
         
-        return self.calculate(nums, 0, 0, S, total, memo)
-
-    def calculate(self, nums, i, sum, S, total, memo):
-        if i == len(nums):
-            return 1 if sum == S else 0
+        dp[0][nums[0] + total] = 1
+        dp[0][-nums[0] + total] += 1
         
-        # Check the memoization table
-        if memo[i][sum + total] != -1:
-            return memo[i][sum + total]
+        for i in range(1, len(nums)):
+            for current_sum in range(-total, total + 1):
+                if dp[i - 1][current_sum + total] > 0:
+                    dp[i][current_sum + nums[i] + total] += dp[i - 1][current_sum + total]
+                    dp[i][current_sum - nums[i] + total] += dp[i - 1][current_sum + total]
         
-        # Calculate ways by adding and subtracting the current number
-        add = self.calculate(nums, i + 1, sum + nums[i], S, total, memo)
-        subtract = self.calculate(nums, i + 1, sum - nums[i], S, total, memo)
-        
-        # Store the result in the memoization table
-        memo[i][sum + total] = add + subtract
-        return memo[i][sum + total]
-
-        
+        return 0 if abs(S) > total else dp[len(nums) - 1][S + total]
